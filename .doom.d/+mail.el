0328c1d60e563274254bdb3ec6c9c69172662ce4
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2020-11-16 11:21:11 +0100

    Fix notmuch popup rules
    messages buffer new have title *notmuch: Subject*

--- .doom.d/+mail.el
+++ .doom.d/+mail.el
@@ -99,6 +99,26 @@
         :map notmuch-message-mode-map
         :n "a" #'mml-attach-file)
 
+  (set-popup-rule! "^\\*notmuch*" :ignore t)
+
+  (defun notmuch-search-show-thread (&optional elide-toggle)
+    "Display the currently selected thread.
+With a prefix argument, invert the default value of
+`notmuch-show-only-matching-messages' when displaying the
+thread."
+    (interactive "P")
+    (let ((thread-id (notmuch-search-find-thread-id))
+          (subject (notmuch-search-find-subject)))
+      (if (> (length thread-id) 0)
+          (notmuch-show thread-id
+                        elide-toggle
+                        (current-buffer)
+                        notmuch-search-query-string
+                        ;; Name the buffer based on the subject.
+                        (concat "*notmuch:"
+                                (truncate-string-to-width subject 30 nil nil t)
+                                "*"))
+        (message "End of search results."))))
 
   ;; (add-hook! 'notmuch-show-hook #'(lambda () (interactive) (visual-fill-column-mode)
   )
