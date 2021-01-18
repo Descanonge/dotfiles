927ff5afdbc36b0c7414318640948be0642aae4d
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2021-01-18 09:17:18 +0100

    Add binding to set git executable manually

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -335,9 +335,12 @@
 
 ;;; TRAMP
 ;; Add path for git on @ciclad
+
 (after! tramp
+  ;; (setq tramp-sh-extra-args '(("/bash\\'" . "-norc")))
   (setenv "SHELL" "/bin/bash")
-  (add-to-list 'tramp-remote-path "/opt/git/2.7.4/bin")
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+  ;; (add-to-list 'tramp-remote-path "/opt/git/2.7.4/bin")
   (add-to-list 'tramp-methods
                '("yadm"
                  (tramp-login-program "yadm")
@@ -346,7 +349,12 @@
                  (tramp-remote-shell "/bin/sh")
                  (tramp-remote-shell-args ("-c"))))
   (map! :leader "g." (cmd! (magit-status  "/yadm::")))
-  )
+
+  (map! :leader
+        (:prefix ("g," . "set exec")
+         :desc "Local" "l" #'(lambda () (interactive) (setq magit-git-executable "git"))
+         :desc "Ciclad" "c" #'(lambda () (interactive) (setq magit-git-executable "/opt/git/2.7.4/bin/git"))
+         )))
 
 ;;; Server
 (server-start)
