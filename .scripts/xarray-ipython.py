5c93501e0a5af0b65a4ffde46022d0489deb54be
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2021-01-21 09:16:06 +0100

    Add script to open nc files in ipython

---
+++ .scripts/xarray-ipython.py
@@ -0,0 +1,34 @@
+# Scripts to open files with Xarray.
+
+import argparse
+import logging
+
+import xarray as xr
+
+
+log = logging.getLogger()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-files', nargs='*', type=str, action='append',
+                        default=([]))
+    dargs = vars(parser.parse_args())
+    return dargs['files'][0]
+
+
+def open_expand_dim(dim='time'):
+    def exp(ds):
+        return ds.expand_dims(dim)
+    return xr.open_mfdataset(globals()['files'], preprocess=exp)
+
+
+if __name__ == '__main__':
+    files = parse_args()
+    try:
+        ds = xr.open_mfdataset(files)
+        print(ds)
+    except Exception as e:
+        log.warning("%s", e.args[0])
+        log.warning("Try 'open_expand_dim()'")
+        log.warning("or 'xr.open_mfdataset(files, ...)'")
