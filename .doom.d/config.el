a8d38ec5db3996f44f69f6bcfc6f57d97a70e023
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2021-02-27 17:49:20 +0100

    Add c++ config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -321,6 +321,25 @@
   (setq direnv-always-show-summary nil)
   )
 
+;;; C++
+(after! flycheck
+  (setq flycheck-cppcheck-include-path '("./include" "../include")))
+
+;; (set-eglot-client! 'c++-mode '("clangd-9" "-j=3" "--clang-tidy"))
+;; (set-eglot-client! 'c++-mode '("ccls" "--init={\"index\": {\"threads\": 3}}"))
+
+(defun chaeck/find-projectile-project (dir)
+  (let ((probe (locate-dominating-file dir ".projectile")))
+    (when probe (cons 'transient probe))))
+
+(add-hook 'project-find-functions 'chaeck/find-projectile-project 'append)
+(setq lsp-clients-clangd-args '("-j=3"
+                                "--background-index"
+                                "--clang-tidy"
+                                "--completion-style=detailed"
+                                "--header-insertion=never"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
+
 ;;; RST
 (use-package! rst
   :config
