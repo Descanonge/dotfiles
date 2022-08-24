5aff09f8e0678484c7f90504d408af69caa72acd
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2022-08-24 19:35:59 +0200

    Update various

--- .scripts/wallpaper-change.sh
+++ .scripts/wallpaper-change.sh
@@ -8,7 +8,7 @@ fi
 
 # xfce
 if [ "$get" == "xfce" ]; then
-    file=$(xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image)
+    file=$(xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/last-image)
 
 # cinnamon
 elif [ "$get" == "cinnamon" ]; then
