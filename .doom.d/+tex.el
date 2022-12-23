8126f5c49880b624b4d456125955c7ff671304c7
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2022-12-23 18:16:32 +0100

    Setup latex for LSP
    It works with texlab v4.3.2 and makefiles such as ones in
    Documents/Work/Thèse

--- .doom.d/+tex.el
+++ .doom.d/+tex.el
@@ -1,23 +1,42 @@
 ;;; +tex.el -*- lexical-binding: t; -*-
 
 
-(setq tex-directory "_build")
+(setq tex-directory "texbuild")
+(setq font-latex-fontify-script nil)
+
+(after! tex
+  (setq TeX-output-dir "texbuild"))
 
 (after! lsp-latex
-  (setq lsp-latex-build-executable "latexmk")
-  (setq lsp-latex-build-aux-directory "_build")
-  (setq lsp-latex-build-args
-        '("-pdflua" "-interaction=nonstopmode" "-synctex=1" "-recorder" "-quiet"
-          "-auxdir=_build" "-emulate-aux-dir" "%f"))
+
+  (setq lsp-latex-build-executable "make"
+        lsp-latex-root-directory "."
+        lsp-latex-build-aux-directory "texbuild"
+        lsp-latex-build-args '())
+  (lsp-latex-setup-variables)
+  (lsp-register-custom-settings
+   `(("texlab.auxDirectory" lsp-latex-build-aux-directory)))
 
   (map! :map LaTeX-mode-map
         :localleader
         "c" :desc "Compile" #'lsp-latex-build)
   )
 
-(after! auctex
-  (setq font-latex-fontify-script nil)
+(after! reftex
+  (setq reftex-cite-format
+        '((?a . "\\autocite[]{%l}")
+          (?b . "\\blockcquote[]{%l}{}")
+          (?c . "\\cite[]{%l}")
+          (?f . "\\footcite[]{%l}")
+          (?n . "\\nocite{%l}")
+          (?P . "\\parencite[]{%l}")
+          (?s . "\\smartcite[]{%l}")
+          (?t . "\\textcite[]{%l}")
+          (?A . "\\citeA[]{%l}")
+          (?p . "\\pcite[]{%l}")))
+  )
 
+(after! auctex
   (add-to-list 'TeX-view-program-list
                '("Zathura"
                  ("zathura %o"
@@ -30,3 +49,18 @@
       (if (file-exists-p (concat tex-directory "/" output-file))
           (TeX-command "View" 'TeX-active-master 0)
         (message "Output file %S does not exist." output-file)))))
+
+
+(defun me/slice-par-line ()
+  "Put a newline for each sentence inside range defined with START and END."
+  (interactive "*")
+  (save-excursion
+    (forward-paragraph)
+    (let ((end (point)))
+      (backward-paragraph)
+      (while (< (point) end)
+        (forward-sentence)
+        (delete-horizontal-space)
+        (unless (= (line-end-position) (point))
+          (newline)))))
+  )
