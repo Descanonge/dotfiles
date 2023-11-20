8ea3ff6228066f6ad24bb3ce06d4b67f9b758b90
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2023-11-20 23:51:56 +0100

    Fix custom action size

--- .config/Thunar/custom_actions/thunar_size.sh
+++ .config/Thunar/custom_actions/thunar_size.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-directory=$@
-
-info=$(du -hc -d0 ${directory[@]} | sort -h)
-
-
-for i in $info; do
-    echo $i
-done | zenity --list \
+du -hc --null -d0 "$@" \
+    | sort -hz \
+    | tr \\t \\0 \
+    | zenity --list \
     --column="Size" --column="Files" \
     --title="Size" --width="500" --height="600"
